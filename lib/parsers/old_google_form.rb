e596ed8184de58ee2526313eec508bc0cf487895
Draco Li <draco@dracoli.com>
2013-10-12 16:38:32 -0400

    Added google old data parser

---
+++ lib/parsers/old_google_form.rb
@@ -0,0 +1,60 @@
+require 'csv'
+module Parsers
+  module OldGoogleForm
+
+    GENERAL_COLUMNS = [
+      'Timestamp',
+      'First Name',
+      'Last Name',
+      'Username'
+    ]
+
+    def self.parse_data(csv,  company)
+      CSV.open(csv, encoding: "UTF-8", headers: true).each do |row|
+
+        # Create this user if not exists without confirmation
+        name = "%s %s" % [row['First Name'], row['Last Name']]
+        if User.where(name: name, company: company).exists?
+          user = User.where(name: name, company: company).first
+        else
+          user = User.new({
+            company: company,
+            name: name,
+            email: row['First Name'][0].downcase + row['Last Name'].downcase + \
+                   '@' + company.email_domain,
+            password: 'password'
+          })
+          user.skip_confirmation!
+          user.save!
+          user.confirm!
+        end
+
+        # Add answers and questions
+        row.each do |k, v|
+          unless  GENERAL_COLUMNS.include? k
+            question = k.strip
+            response = v
+
+            # Get/create question
+            ques_data = {
+              company: company,
+              content: question
+            }
+            ques = Question.where(ques_data).first_or_create({
+              added_source: 'csv'
+            })
+
+            # Get/create and update response
+            ans = Answer.where({
+              user: user,
+              question: ques
+            }).first_or_create
+            ans.content = response
+            ans.save
+          end
+        end
+      end
+    end
+
+  end
+end
