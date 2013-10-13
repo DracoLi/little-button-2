10a2cd8322a11375b8b26a1cd16755c5095c95ad
Draco Li <draco@dracoli.com>
2013-10-13 03:58:27 -0400

    Now autoloading email processor through initializer

--- config/initializers/griddler.rb
+++ config/initializers/griddler.rb
@@ -2,3 +2,5 @@ Griddler.configure do |config|
   config.reply_delimiter = '-- REPLY ABOVE THIS LINE --'
   config.email_service = :mandrill # :sendgrid, :cloudmailin, :postmark, :mandrill, :mailgun
 end
+
+require "#{Rails.root}/lib/email_processor.rb"
